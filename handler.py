e31d2b0febb63f45a65d5956fde8140fbb4cadbb
angeloemd <angeloemd@gmail.com>
2026-08-04 06:33:12 -0600

    Update handler.py

--- handler.py
+++ handler.py
@@ -61,7 +61,7 @@ def probe_duration_seconds(path):
     out = subprocess.run(
         [
             "ffprobe", "-v", "error", "-show_entries", "format=duration",
-            "-of", "default=noprint_wrapper=1:nokey=1", path,
+            "-of", "default=noprint_wrappers=1:nokey=1", path,
         ],
         capture_output=True, text=True, check=True,
     )
